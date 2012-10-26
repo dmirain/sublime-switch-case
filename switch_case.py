b98e5a8bc654b6eff1ddb2338ff4d0fea1ea147e
Dmitry Prokofyev <dmirain@yandex-team.ru>
2012-10-26 23:04:03 +0400

    Переписал на регекспы. Добавил переключение в camel формат с первой большой буквой.

--- switch_case.py
+++ switch_case.py
@@ -3,50 +3,49 @@
 import sublime
 import sublime_plugin
 
+import re
 
-def detect_case(text):
+class UnknownCase(Exception):
+    pass
+
+def switch_case(text):
     '''
-        Если текст состоит из букв, цифр и _, то это underscore.
-        Если текст состоит из букв, цифр, то это camel.
+        Если текст состоит из маленьких букв, цифр и _, то это underscore.
+            Underscore переключаем в camel_f.
+        Если текст состоит из букв, цифр и начинается на маленькую букву,
+            то это camel_f. Сamel_f переключаем в camel_c.
+        Если текст состоит из букв, цифр и начинается на большую букву,
+            то это camel_c. Сamel_c переключаем в underscore.
     '''
-    replaced = text.replace('_', '')
-    if not replaced.isalnum():
-        return 'other'
+    if re.match(r"^[a-z][A-Za-z0-9]+$", text):
+        return switch_case_to_camel_c_case(text)
+    elif re.match(r"^[A-Z][A-Za-z0-9]+$", text):
+        return switch_case_to_underscore_case(text)
+    elif re.match(r"^[a-z][a-z0-9_]+$", text):
+        return switch_case_to_camel_f_case(text)
+    else:
+        raise UnknownCase
 
-    if text.find('_') != -1:
-            return 'underscore'
-    return 'camel'
+def switch_case_to_underscore_case(text):
+    rep = lambda m: (m.group(1) and m.group(1) + '_') + m.group(2).lower()
+    return re.sub(r"(^|[a-z0-9])([A-Z]+)", rep, text)
 
+def switch_case_to_camel_f_case(text):
+    return re.sub(r"_([a-z0-9])", lambda m: m.group(1).capitalize(), text)
 
-def translate_to_underscore_case(text):
-    underscored = ''
-    for ch in text:
-        if ch.isupper():
-            underscored += '_'
-        underscored += ch
-    return underscored.lower().strip('_')
-
-
-def translate_to_camel_case(text):
-    words = map(lambda x: x.capitalize(), text.split('_'))
-    words[0] = words[0].lower()
-    return ''.join(words)
-
+def switch_case_to_camel_c_case(text):
+    return re.sub(r"^([a-z0-9])", lambda m: m.group(1).capitalize(), text)
 
 class SwitchCaseCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
             text = self.view.substr(region)
+
             if not text:
                 return
 
-            case = detect_case(text)
-            if case == 'camel':
-                text = translate_to_underscore_case(text)
-            elif case == 'underscore':
-                text = translate_to_camel_case(text)
-            else:
+            try:
+                text = switch_case(text)
+                self.view.replace(edit, region, text)
+            except UnknownCase:
                 sublime.status_message('Unknown case type')
-                return
-
-            self.view.replace(edit, region, text)
